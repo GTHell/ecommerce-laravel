192fbf75631925eda4fc651385ad01cf89c55e3a
GTHell <sosxradar@gmail.com>
2018-08-02 04:09:21 +0700

    Add ProductionOptions Model Modify options migration

--- database/migrations/2018_08_01_210848_create_product_options_table.php
+++ database/migrations/2018_08_01_210848_create_product_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOptionsTable extends Migration
+class CreateProductOptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateOptionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('options', function (Blueprint $table) {
+        Schema::create('product_options', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('group_name');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateOptionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('options');
+        Schema::dropIfExists('product_options');
     }
 }
